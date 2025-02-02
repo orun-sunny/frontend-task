f454252fd8a910ed437e493d8f170754feb6135f
orun-sunny <sunny.mukut@gmail.com>
2025-02-02 18:56:53 +0600

    fix navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "./button";
 import * as path from "@/paths";
 import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
 import {
   motion,
   useMotionValueEvent,
@@ -11,7 +12,6 @@ import {
   AnimatePresence,
 } from "motion/react";
 import BrandLogo from "./brand-logo";
-import Image from "next/image";
 
 interface NavigationItem {
   label: string;
@@ -94,8 +94,8 @@ export function Header() {
       )}
     >
       <div className="container">
-        <div className="flex items-center justify-between relative">
-          <div className="flex-1 shrink-0">
+        <div className="flex items-center relative">
+          <div className="flex items-center flex-1 shrink-0">
             <Link className="text-white" href="/">
               <BrandLogo />
             </Link>
@@ -132,33 +132,48 @@ function NavigationMenu({ items, position }: NavigationMenuProps) {
         <li className="relative group" key={navItem.label}>
           <Link
             className={cn(
-              "cursor-pointer font-medium py-3 px-4 relative after:absolute after:inset-x-0 after:h-px after:bg-transparent after:bottom-0 hover:after:bg-white after:transition-colors after:duration-200"
+              "cursor-pointer font-medium py-3 px-4 relative after:absolute after:inset-x-5 after:h-px after:bg-transparent after:bottom-0 hover:after:bg-white after:transition-all translate-y-4  flex items-center"
             )}
             href={navItem.href()}
           >
             {navItem.label}
+            {navItem.children.length > 0 && (
+              <ChevronDown className="w-4 h-4 transition-transform duration-200 group-hover:rotate-180" />
+            )}
           </Link>
           {navItem.children && <SubmenuList items={navItem.children} />}
         </li>
       ))}
-      <li>
-        <button
-          type="button"
-          className={cn(
-            "rounded-full border py-2 px-7 mx-4",
-            position === "fixed" && "text-secondary"
-          )}
-        >
-          Eng
-        </button>
-      </li>
+      <Intl position={position} />
     </>
+  );
+}
+
+function Intl({ position }: { position: string }) {
+  return (
+    <li className="relative group">
+      <button
+        className={cn(
+          "rounded-full border py-2 px-7 mx-4 cursor-pointer",
+          position === "fixed" && "text-secondary"
+        )}
+      >
+        Eng
+      </button>
+
+      <motion.div className="absolute top-16 flex flex-col [&>li]:py-0.5 bg-white shadow w-60 text-gray-900 px-4 [&>*]:py-2 [&>*:not(:last-child)]:border-b [&>*:not(:last-child)]:border-b-gray-300 opacity-0 invisible group-hover:visible group-hover:opacity-100 translate-y-4 group-hover:-translate-y-2 transition-all duration-150">
+        <span>EN (English)</span>
+        <span>TN (Thai)</span>
+        <span>ID (Bahasa Indonesia)</span>
+        <span>TW (Traditional Chinese)</span>
+      </motion.div>
+    </li>
   );
 }
 
 function SubmenuList({ items }: SubmenuListProps) {
   return (
-    <ul className="absolute group-hover:opacity-100 group-hover:visible opacity-0 invisible top-8 bg-white whitespace-nowrap w-52 [&>li]:px-4 [&>li]:py-2.5 [&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-gray-300 rounded shadow text-gray-900">
+    <ul className="absolute group-hover:opacity-100 group-hover:visible opacity-0 invisible top-18 bg-white whitespace-nowrap w-52 [&>li]:px-4 [&>li]:py-2.5 [&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-gray-300 rounded shadow text-gray-900">
       {items.map((submenuItem) => (
         <li key={submenuItem.label}>
           <Link href={submenuItem.href()}>{submenuItem.label}</Link>
@@ -173,26 +188,44 @@ function MobileNav({ position }: { position: string }) {
   return (
     <>
       <button
-        className={cn(position === "fixed" && "text-secondary")}
+        className={cn(
+          "flex items-center justify-center cursor-pointer",
+          position === "fixed" && "text-secondary"
+        )}
         type="button"
         onClick={() => setOpen(!open)}
       >
-        menu
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
       </button>
       <AnimatePresence mode="wait">
         {open && (
           <motion.div
-            initial={{ x: -100 }}
-            animate={{ x: open ? 0 : -100 }}
-            className="fixed inset-x-0 text-white bg-secondary top-16 py-8 shadow"
+            initial={{ x: "-100%" }}
+            animate={{ x: open ? 0 : "-100%" }}
+            exit={{ x: "-100%" }}
+            transition={{ type: "tween", ease: "easeInOut", duration: 0.3 }}
+            className="fixed inset-0 text-white bg-secondary top-16 py-8 shadow overflow-y-auto"
           >
-            <div className="container space-y-8">
-              <ul className={cn("[&_a]:!text-white space-y-2")}>
+            <div className="container space-y-12">
+              <ul className={cn("[&_a]:!text-white space-y-4")}>
                 {navigationItems.map((item) => (
                   <li key={item.label}>
                     <Link href={item.href()}>{item.label}</Link>
                     {item.children && (
-                      <ul className="pl-4 space-y-2">
+                      <ul className="pl-4 mt-2 space-y-2">
                         {item.children.map((ch) => (
                           <li key={ch.label}>
                             <Link href={ch.href()}>{ch.label}</Link>
@@ -203,16 +236,9 @@ function MobileNav({ position }: { position: string }) {
                   </li>
                 ))}
               </ul>
-              <div className="w-full sm:px-20">
-                <Button className="w-full">
+              <div className="w-full px-4 sm:px-20">
+                <Button variant="outline" className="w-full h-12 text-base">
                   Contact Us
-                  <Image
-                    src="assets/icons/ChevronRightWhite.svg"
-                    alt="chevron right"
-                    width={5}
-                    height={5}
-                    className={"h-3.5 w-3.5 mt-1 transition-all duration-300"}
-                  />
                 </Button>
               </div>
             </div>
